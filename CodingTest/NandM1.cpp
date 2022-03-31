9953e8078cdd4b3ffe049ebec0988f91e0dbbb64
YuShinSong <dbtls6513@naver.com>
2022-03-31 17:34:57 +0900

    baekjoon-NandM1 solution

---
+++ CodingTest/NandM1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9] = {0, };
+bool visited[9] = {0, };
+
+void dfs(int num)
+{
+	if (num == M) // 순열을 완성했을 때
+	{
+		for (int i = 0; i < M; ++i)
+			cout << arr[i] << " ";
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; ++i)
+	{
+		if (!visited[i]) // 아직 들르지 않았을 때
+		{
+			visited[i] = true;
+			arr[num] = i;
+			dfs(num + 1);
+			visited[i] = false;
+		}
+	}
+}
+int main()
+{
+	cin >> N >> M;
+
+	dfs(0);
+}
