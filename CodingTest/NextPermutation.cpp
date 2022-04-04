5a079c86dd621e787f8964ca9e283e16798a6d96
YuShinSong <dbtls6513@naver.com>
2022-04-04 14:47:13 +0900

    baekjoon-NextPermutation solution

---
+++ CodingTest/NextPermutation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+void solution(int N, vector<int> nums)
+{
+	if (next_permutation(nums.begin(), nums.end()))
+	{
+		for (int i : nums)
+			cout << i << " ";
+	}
+	else
+		cout << -1;
+}
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> nums;
+
+	for (int i = 0; i < N; ++i)
+	{
+		int temp;
+		cin >> temp;
+		nums.push_back(temp);
+	}
+
+	solution(N, nums);
+}
